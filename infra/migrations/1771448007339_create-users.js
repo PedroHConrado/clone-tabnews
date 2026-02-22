5a377fd66a5d763ad85fdd09165d299021dd2cae
Pedro Conrado <conradohpedro@gmail.com>
2026-02-22 12:01:09 -0300

    feat: create `user` model and `/api/v1/users` endpoint

--- infra/migrations/1771448007339_create-users.js
+++ infra/migrations/1771448007339_create-users.js
@@ -21,7 +21,6 @@ exports.up = (pgm) => {
     password: {
       type: "varchar(60)",
       notNull: true,
-      unique: true,
     },
     // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
     created_at: {
