41098a4e1a2f0c3783ceb4970650a32aa9e182ca
Pedro Conrado <conradohpedro@gmail.com>
2026-03-03 16:23:42 -0300

    feat: hash `password` in `user.create`

--- models/user.js.js
+++ models/user.js.js
@@ -1,4 +1,5 @@
 import database from "infra/database";
+import password from "models/password.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function findOneByUsername(username) {
@@ -33,6 +34,7 @@ async function findOneByUsername(username) {
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
   await validateUniqueUsername(userInputValues.username);
+  await hashPasswordInObject(userInputValues);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
@@ -56,6 +58,7 @@ async function create(userInputValues) {
       });
     }
   }
+
   async function validateUniqueUsername(username) {
     const results = await database.query({
       text: `
@@ -75,6 +78,12 @@ async function create(userInputValues) {
       });
     }
   }
+
+  async function hashPasswordInObject(userInputValues) {
+    const hashedPassword = await password.hash(userInputValues.password);
+    userInputValues.password = hashedPassword;
+  }
+
   async function runInsertQuery(userInputValues) {
     const results = await database.query({
       text: `
